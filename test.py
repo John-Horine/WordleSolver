a39ceed6ce9cf3d67c45ef7e8cb30b885b9d4374
John-Horine <100107903+John-Horine@users.noreply.github.com>
2022-02-20 22:50:24 -0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from typing import Counter
-
-
-myDict = Counter({4: 949, 3: 944, 5: 233, 2: 131, 6: 43, 7: 14, 1: 1})
-import collections as c
-
-air = c.OrderedDict(sorted(myDict.items()))
-print(air)
